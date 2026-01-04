92593e8d462df29731caff8c6d26ff229028a8ac
NavyaArora24 <aroranavya78@gmail.com>
2026-01-04 19:39:30 +0530

    login redone

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
-import ThemeBtn from './components/Themebtn'
+import ThemeBtn from './components/ThemeBtn'
 import { ThemeProvider } from './Context/theme'
 
 function App() {
--- 09themeSwitcher/src/Context/theme.js
+++ 09themeSwitcher/src/Context/theme.js
@@ -1,7 +1,11 @@
 import {createContext, useContext} from 'react';
 
-export const ThemeContext = createContext({themeMode : 'light', darkTheme: () => {},
-lighTheme: () => {},})
+export const ThemeContext = createContext({
+  themeMode: "light",
+  darkTheme: () => {},
+  lightTheme: () => {}, // ✅ fixed
+});
+
 
 export const ThemeProvider = ThemeContext.Provider;
 
